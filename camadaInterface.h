088558967c76ef85602eb36b0773bb15eb98fc1f
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-03-11 19:36:22 +0000

    Update camadaInterface.h

--- camadaInterface.h
+++ camadaInterface.h
@@ -1,9 +1,6 @@
-//
-// Created by josejoao on 09/03/20.
-//
-
-#ifndef PROJETO_CAMADAINTERFACE_H
-#define PROJETO_CAMADAINTERFACE_H
+#ifndef PROJETO_LI2_CAMADAINTERFACE_H
+#define PROJETO_LI2_CAMADAINTERFACE_H
 void mostrar_tabuleiro (ESTADO *e);
 int interpretador(ESTADO *e);
-#endif //PROJETO_CAMADAINTERFACE_H
+
+#endif //PROJETO_LI2_CAMADAINTERFACE_H
