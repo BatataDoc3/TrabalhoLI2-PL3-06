41236c7269976d8e6d36cc420f934bdf9e50f351
BatataDoc3 <61985897+BatataDoc3@users.noreply.github.com>
2020-03-11 17:47:16 +0000

    Update estrutura.c

--- estrutura.c
+++ estrutura.c
@@ -4,11 +4,8 @@
 
 #define BUF_SIZE 1024
 
-
-
-
 typedef enum {VAZIO, BRANCA, PRETA} CASA;
-typedef struct {int coluna;int linha;} COORDENADA;
+typedef struct {int linha;int coluna;} COORDENADA;
 typedef struct {COORDENADA jogador1;
     COORDENADA jogador2;
 } JOGADA;
@@ -20,13 +17,46 @@ typedef struct {CASA tab[8][8];
     int jogador_atual;
 } ESTADO;
 
+CASA obter_estado_casa (ESTADO *e, COORDENADA *x){
+    CASA casa;
+    int coluna, linha;
+    coluna = x -> coluna;
+    linha = x -> linha;
+    casa = e -> tab [linha] [coluna] ;
+    return casa;
+}
 
+int obter_numero_de_jogadas (ESTADO *e) {
+    int num_jogadas;
+    num_jogadas = e -> num_jogadas;
+    return num_jogadas ;
+}
 
-
+int obter_jogador_atual (ESTADO *e) {
+    int jogadorAtual;
+    jogadorAtual = e -> jogador_atual;
+    return jogadorAtual;
+}
 
 
 // Função que deve ser completada e colocada na camada de dados
 ESTADO *inicializar_estado() {
+    int linha, coluna;
+    ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
+    e->jogador_atual = 1;
+    e->num_jogadas = 0;
+    for (linha = 0; linha < 8 ; linha ++) {
+        for (coluna = 0; coluna < 8; coluna ++) {
+            if (linha == 3 && coluna == 4) 
+            e -> tab [linha] [coluna] = BRANCA;
+            else 
+            e -> tab [linha] [coluna] = VAZIO;
+        }
+    }
+    e -> ultima_jogada.coluna = 4;
+    e -> ultima_jogada.linha = 3;
+    return e ;
+}
     int linha, coluna;
     ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
     e->jogador_atual = 1;
