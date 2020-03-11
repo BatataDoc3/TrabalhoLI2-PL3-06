a4b918169338d1453c8c4251cfa01e14f4dcc40e
José João Cardoso Gonçalves   a93204 <61986130+jjgonc@users.noreply.github.com>
2020-03-11 18:47:23 +0000

    Update logicaProg.c

--- logicaProg.c
+++ logicaProg.c
@@ -1,5 +1,8 @@
+#include "estrutura.h"
+#include <stdio.h>
+
 int jogar(ESTADO *e, COORDENADA c) {
-printf("jogar %d %d\n", c.coluna, c.linha);
-e -> tab [c.coluna] [c.linha] = BRANCA;
-return 1;
+    printf("jogar %d %d\n", c.coluna, c.linha);
+    e -> tab [c.coluna] [c.linha] = BRANCA;
+    return 1;
 }
