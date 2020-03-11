be71f56b22db8b772f3967bac88d21a6fd3039b1
BatataDoc3 <61985897+BatataDoc3@users.noreply.github.com>
2020-03-11 17:47:48 +0000

    Update logicaProg.c

--- logicaProg.c
+++ logicaProg.c
@@ -1,11 +1,5 @@
-//
-// Created by josejoao on 09/03/20.
-//
-
-ESTADO *inicializar_estado() {
-
-ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
-e->jogador_atual = 1;
-e->num_jogadas = 0;
-// Falta a resto da inicialização.
-return e;}
+int jogar(ESTADO *e, COORDENADA c) {
+printf("jogar %d %d\n", c.coluna, c.linha);
+e -> tab [c.coluna] [c.linha] = BRANCA;
+return 1;
+}
