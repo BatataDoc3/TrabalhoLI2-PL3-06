0ac80541c337d60ca19de3fb38ccdcb627b1b438
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-03-11 19:33:11 +0000

    Update estrutura.c

--- estrutura.c
+++ estrutura.c
@@ -1,24 +1,5 @@
-#include <stdio.h>
+#include "estrutura.h"
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-#define BUF_SIZE 1024
-
-typedef enum {VAZIO, BRANCA, PRETA} CASA;
-typedef struct {int linha;int coluna;} COORDENADA;
-typedef struct {COORDENADA jogador1;
-    COORDENADA jogador2;
-} JOGADA;
-typedef JOGADA JOGADAS[32];
-typedef struct {CASA tab[8][8];
-    COORDENADA ultima_jogada;
-    JOGADAS jogadas;
-    int num_jogadas;
-    int jogador_atual;
-} ESTADO;
-
-
 
 
 CASA obter_estado_casa (ESTADO *e, COORDENADA *x){
