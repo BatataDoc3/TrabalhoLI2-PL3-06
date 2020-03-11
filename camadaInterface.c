73e8dfd69dc528c22c3e0366b596473628c4924a
BatataDoc3 <61985897+BatataDoc3@users.noreply.github.com>
2020-03-11 17:46:25 +0000

    Update camadaInterface.c

--- camadaInterface.c
+++ camadaInterface.c
@@ -1,13 +1,23 @@
-//
-// Created by josejoao on 09/03/20.
-//
-
 int interpretador(ESTADO *e) {
     char linha[BUF_SIZE];
     char col[2], lin[2];
-    if(fgets(linha, BUF_SIZE, stdin) == NULL)return 0;
-    if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {COORDENADA coord = {*col -'a', *lin -'1'};
+    if(fgets(linha, BUF_SIZE, stdin) == NULL) return 0;
+    if(strlen(linha) == 3 && sscanf(linha, "%[0-7]%[0-7]", col, lin) == 2) {COORDENADA coord = {*col -'0', *lin -'0'};
     jogar(e, coord);mostrar_tabuleiro(e);
     }
     return 1;
+}
+
+void mostrar_tabuleiro (ESTADO *e) {
+    int linha,coluna;
+    for (linha = 0; linha < 8; linha ++) {
+        for (coluna = 0; coluna < 8; coluna ++) {
+            if (e -> tab [0] [7]) putchar ('2');
+            else if (e-> tab [7] [0]) putchar ('1');
+            else if (e -> tab [linha] [coluna]  == VAZIO) putchar ('.') ;
+            else if (e -> tab [linha] [coluna]  == BRANCA) putchar ('*');
+            else putchar ('#');
+        }
+        printf ("\n");
+    }
 }
