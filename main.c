077cad2f21e1139125f603da2c3971eab4dc7149
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-03-15 21:45:48 +0000

    Update main.c

--- main.c
+++ main.c
@@ -5,11 +5,8 @@
 int main(){
     ESTADO *e = inicializar_estado();
     mostrar_tabuleiro(e);
-    int num_jogadas;
-    num_jogadas = obter_numero_de_jogadas(e);
-    while (num_jogadas <32) {
+    while (e -> num_jogadas <32) {
         interpretador(e);
-        num_jogadas++;
     }
     return 0;
 }
