4e1d0ce80a546d634c13cff1a16103b44aba2c4c
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-03-15 21:47:45 +0000

    Update logicaProg.c

--- logicaProg.c
+++ logicaProg.c
@@ -4,5 +4,7 @@
 int jogar(ESTADO *e, COORDENADA c) {
     printf("jogar %d %d\n", c.coluna, c.linha);
     e -> tab [c.coluna] [c.linha] = BRANCA;
+    e -> num_jogadas = atualizar_num_jogadas(e);
+    e -> jogador_atual = atualizar_jogador_atual (e);
     return 1;
 }
