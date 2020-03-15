9095f593f36e3868fb6f60932adf6b9f52d4709d
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-03-15 18:56:19 +0000

    Update camadaInterface.c
    Neste commit alterei a função mostrar_tabuleiro, para considerar as linhas de 8 a 1 visto que anteriormente tínhamos considerado as linhas de 1 a 8.

--- camadaInterface.c
+++ camadaInterface.c
@@ -7,10 +7,10 @@
 
 void mostrar_tabuleiro (ESTADO *e) {
     int linha,coluna;
-    for (linha = 0; linha < 8; linha ++) {
+    for (linha = 7; linha >= 0; linha--) {
         for (coluna = 0; coluna < 8; coluna ++) {
-            if (linha == 0 && coluna ==7) putchar ('2');
-            else if (linha == 7 && coluna == 0) putchar ('1');
+            if (linha == 7 && coluna ==7) putchar ('2');
+            else if (linha == 0 && coluna == 0) putchar ('1');
             else if (e -> tab [linha] [coluna]  == VAZIO) putchar ('.') ;
             else if (e -> tab [linha] [coluna]  == BRANCA) putchar ('*');
             else putchar ('#');
@@ -24,10 +24,15 @@ void mostrar_tabuleiro (ESTADO *e) {
 int interpretador(ESTADO *e) {
     char linha[BUF_SIZE];
     char col[2], lin[2];
-    if(fgets(linha, BUF_SIZE, stdin) == NULL) return 0;
-    if(strlen(linha) == 3 && sscanf(linha, "%[0-7]%[0-7]", col, lin) == 2) {COORDENADA coord = {*col -'0', *lin -'0'};
+
+    if(fgets(linha, BUF_SIZE, stdin) == NULL)
+        return 0;
+
+    if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
+        COORDENADA coord = {*col - 'a', *lin - '1'};
         jogar(e, coord);
         mostrar_tabuleiro(e);
     }
     return 1;
 }
+
