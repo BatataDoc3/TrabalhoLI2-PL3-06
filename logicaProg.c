73c40129880e488a5380f517731b475e71bf3fa1
José João Cardoso Gonçalves   a93204 <61986130+jjgonc@users.noreply.github.com>
2020-03-19 22:55:37 +0000

    Update logicaProg.c

--- logicaProg.c
+++ logicaProg.c
@@ -2,12 +2,18 @@
 #include <stdio.h>
 
 int jogar(ESTADO *e, COORDENADA c) {
-    if (jogada_valida (e,c) == 1) {
+    int x;
+    if (jogada_valida(e, c) == 1) {
         printf("jogar %d %d\n", c.coluna, c.linha);
         atualizar_casa_preta(e);
         atualizar_casa(e, c);
         e->num_jogadas = atualizar_num_jogadas(e);
         e->jogador_atual = atualizar_jogador_atual(e);
+        if (jogo_finalizado(e, c) == 1){
+            x = parabens_jogador(e);
+            printf("Jogo Finalizado! Parabéns Jogador %d\n", x);
+            return 0;
+        }
         return 1;
     }
     else {
