9c3edc3fa99978b582010aaf9befc2f53ea0bde0
Jorge Miguel Silva Melo a93308 <61985897+BatataDoc3@users.noreply.github.com>
2020-03-19 19:50:41 +0000

    Agora a jogar verifica se a jogada é válida
    Agora a função não muda o estado, em vez disso, chama a função atualizar_casa

--- logicaProg.c
+++ logicaProg.c
@@ -2,9 +2,16 @@
 #include <stdio.h>
 
 int jogar(ESTADO *e, COORDENADA c) {
-    printf("jogar %d %d\n", c.coluna, c.linha);
-    e -> tab [c.coluna] [c.linha] = BRANCA;
-    e -> num_jogadas = atualizar_num_jogadas(e);
-    e -> jogador_atual = atualizar_jogador_atual (e);
-    return 1;
+    if (jogada_valida (e,c) == 1) {
+        printf("jogar %d %d\n", c.coluna, c.linha);
+        atualizar_casa_preta(e);
+        atualizar_casa(e, c);
+        e->num_jogadas = atualizar_num_jogadas(e);
+        e->jogador_atual = atualizar_jogador_atual(e);
+        return 1;
+    }
+    else {
+        printf ("Jogada inválida\n");
+        return 0;
+    }
 }
