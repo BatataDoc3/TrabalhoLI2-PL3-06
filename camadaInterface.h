3f60c76eec985f8712096c1b167cf421bc47cf03
Jorge Miguel Silva Melo a93308 <61985897+BatataDoc3@users.noreply.github.com>
2020-03-20 16:10:46 +0000

    Adicionada documentação

--- camadaInterface.h
+++ camadaInterface.h
@@ -1,6 +1,21 @@
+/**
+ * @file camadaInterface.h
+ * \brief conjunto de funções que desenham o tabuleiro e recebem o input do jogador, e fazem várias ações dependendo do input
+ */
+
 #ifndef PROJETO_LI2_CAMADAINTERFACE_H
 #define PROJETO_LI2_CAMADAINTERFACE_H
+/**
+ * \brief Função que atualiza o desenho do tabuleiro.
+ * @param e Apontador para o ESTADO.
+ */
 void mostrar_tabuleiro (ESTADO *e);
+
+/**
+ * Função que lê os inputs e efetua as Jogadas no tabuleiro.
+ * @param e Apontador para o ESTADO.
+ * @return
+ */
 int interpretador(ESTADO *e);
 
 #endif //PROJETO_LI2_CAMADAINTERFACE_H
