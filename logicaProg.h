56b5c7c188509e20cd85837e08934ae808fb6ad7
Jorge Miguel Silva Melo a93308 <61985897+BatataDoc3@users.noreply.github.com>
2020-03-20 16:04:42 +0000

    Adicionada documentação e mudança de funções para este módulo

--- logicaProg.h
+++ logicaProg.h
@@ -1,7 +1,54 @@
+/**
+ * @file logicaProg.h
+ * \brief conjunto de funções que verificam se a jogada é válida e faz a jogada
+ */
+
 #ifndef PROJETO_LI2_LOGICAPROG_H
 #define PROJETO_LI2_LOGICAPROG_H
 
 #include "estrutura.h"
+
+/**
+ * Função que verifica as Jogadas e analisa quando o jogo acaba.
+ * @param e Apontador para o ESTADO.
+ * @param c Recebe uma COORDENADA para onde pretende ir.
+ * @return
+ */
+
 int jogar(ESTADO *e, COORDENADA c);
+
+/**
+ * \brief Função que verifica se uma jogada é válida
+ * @param e pointer do estado
+ * @param c coordenada
+ * @return 1 caso seja válida, (-1) caso não seja
+ */
 int jogada_valida (ESTADO *e, COORDENADA c);
+
+int jogo_finalizado (ESTADO *e, COORDENADA c);
+
+/**
+ * \brief Função que verifica se o jogador está rodeado de peças pretas
+ * @param e pointer do estado
+ * @param c coordenada
+ * @return 1 se estiver rodeado de pretas ou 0 se não
+ */
+int rodeado_pretas (ESTADO *e, COORDENADA c);
+
+/**
+ * \brief Função que verifica se o jogador está rodeado de peças pretas
+ * @param e pointer do estado
+ * @param c coordenada
+ * @return 1 se estiver rodeado de pretas ou 0 se não
+ */
+int casa_Preta (ESTADO*e, COORDENADA c);
+
+/**
+ * \brief Função que diz qual é o jogador vencedor
+ * @param e pointer do estado
+ * @return jogador vencedor
+ */
+int parabens_jogador (ESTADO *e);
+
+
 #endif //PROJETO_LI2_LOGICAPROG_H
