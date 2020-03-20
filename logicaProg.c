32217430ff7e2b9a795e73d3c89c827490a968a6
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-03-20 15:17:22 +0000

    Update logicaProg.c

--- logicaProg.c
+++ logicaProg.c
@@ -1,5 +1,32 @@
+
 #include "estrutura.h"
 #include <stdio.h>
+
+int jogada_valida_pecaBranca (ESTADO*e, COORDENADA c) {
+    int linha,coluna;
+    linha = c.linha;
+    coluna = c.coluna;
+    if (e -> tab [linha+1] [coluna] == '*' ||
+        e -> tab [linha] [coluna + 1] == '*' ||
+        e -> tab [linha + 1] [coluna + 1] == '*' ||
+        e -> tab [linha - 1] [coluna] == '*' ||
+        e -> tab [linha] [coluna - 1] == '*' ||
+        e -> tab [linha - 1] [coluna - 1] == '*' ||
+        e -> tab [linha - 1] [coluna + 1] == '*' ||
+        e -> tab [linha + 1] [coluna - 1] == '*')
+        return 1;
+    else return 0;
+}
+
+int jogada_valida (ESTADO *e , COORDENADA c) {
+    if (jogada_valida_pecaBranca(e,c) == 1) {
+        if (e-> tab [c.linha] [c.coluna] == '#')
+            return (-1);
+        else return 1;
+    }
+    else return (-1);
+}
+
 
 int jogar(ESTADO *e, COORDENADA c) {
     int x;
@@ -12,6 +39,7 @@ int jogar(ESTADO *e, COORDENADA c) {
         if (jogo_finalizado(e, c) == 1){
             x = parabens_jogador(e);
             printf("Jogo Finalizado! Parabéns Jogador %d\n", x);
+            e -> num_jogadas = 32;
             return 0;
         }
         return 1;
