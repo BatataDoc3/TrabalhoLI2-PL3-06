a4bb247f77bc86b9acbe42a4ecf9037885b61a38
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-03-31 16:05:53 +0100

    Update camadaInterface.c

--- camadaInterface.c
+++ camadaInterface.c
@@ -5,7 +5,7 @@
 
 void mostrar_tabuleiro (ESTADO *e) {
     int linha,coluna;
-    char letra = 'H';
+    char letra = '8';
     for (linha = 7; linha >= 0; linha--) {
         putchar (letra) ;
         letra--;
@@ -18,7 +18,7 @@ void mostrar_tabuleiro (ESTADO *e) {
         }
         printf ("\n");
     }
-    printf ("  1 2 3 4 5 6 7 8 \n");
+    printf ("  A B C D E F G H \n");
 }
 
 
@@ -41,10 +41,10 @@ void print_array (FILE *f, ESTADO *e){
     for (i=0;i <= (obter_numero_de_jogadas(e)); i++ ){
         if (i<9){
             if ((e->jogadas[i].jogador1.linha)!= 0 || e->jogadas[i].jogador1.coluna != 0) {
-                fprintf(f, "0%d: %c%d ", i + 1, (e->jogadas[i].jogador1.linha) + 'a', e->jogadas[i].jogador1.coluna + 1);
+                fprintf(f, "0%d: %c%d ", i + 1, (e->jogadas[i].jogador1.coluna) + 'a', e->jogadas[i].jogador1.linha + 1);
             }
             if ((e->jogadas[i].jogador2.linha)!= 0 || e->jogadas[i].jogador2.coluna != 0) {
-                fprintf(f, "%c%d ", (e->jogadas[i].jogador2.linha) + 'a', e->jogadas[i].jogador2.coluna + 1);
+                fprintf(f, "%c%d ", (e->jogadas[i].jogador2.coluna) + 'a', e->jogadas[i].jogador2.linha + 1);
                 fprintf(f, "\n");
             }
             if (((e->jogadas[i].jogador1.linha)!= 0 || e->jogadas[i].jogador1.coluna != 0) && ((e->jogadas[i].jogador2.linha)== 0 && e->jogadas[i].jogador2.coluna == 0)){
@@ -53,10 +53,10 @@ void print_array (FILE *f, ESTADO *e){
         }
         else {
             if ((e->jogadas[i].jogador1.linha)!= 0 || e->jogadas[i].jogador1.coluna != 0) {
-                fprintf(f, "%d: %c%d ", i + 1, (e->jogadas[i].jogador1.linha) + 'a', e->jogadas[i].jogador1.coluna + 1);
+                fprintf(f, "%d: %c%d ", i + 1, (e->jogadas[i].jogador1.coluna) + 'a', e->jogadas[i].jogador1.linha + 1);
             }
             if ((e->jogadas[i].jogador2.linha)!= 0 || e->jogadas[i].jogador2.coluna != 0) {
-                fprintf(f, "%c%d ", (e->jogadas[i].jogador2.linha) + 'a', e->jogadas[i].jogador2.coluna + 1);
+                fprintf(f, "%c%d ", (e->jogadas[i].jogador2.coluna) + 'a', e->jogadas[i].jogador2.linha + 1);
                 fprintf(f, "\n");
             }
             if (((e->jogadas[i].jogador1.linha)!= 0 || e->jogadas[i].jogador1.coluna != 0) && ((e->jogadas[i].jogador2.linha) ==  0 || e->jogadas[i].jogador2.coluna == 0)){
@@ -71,18 +71,19 @@ int gravar (ESTADO *e,char *filename) {
     if (f == NULL)
         return 0;
     gravar_tabuleiro(f,e);
+    fprintf (f,"\n");
     print_array(f,e);
     fclose(f);
     return 1;
 }
 
-int ler (ESTADO *e,char *filename, int a, int b){
+int ler (ESTADO *e,char *filename){
     FILE *f = fopen (filename, "r");
     if (f == NULL){
         printf ("Erro ao abrir ficheiro\n");
         return 0;
     }
-    le_ficheiro(e, f, a, b);
+    le_ficheiro(e, f);
     mostrar_tabuleiro(e);      //caso nao seja para mostrar o tabuleiro ao fazer a função ler, tirar esta função.
     return 1;
 }
@@ -91,24 +92,21 @@ int interpretador(ESTADO *e) {
     char linha[BUF_SIZE];
     char col[2], lin[2];
     char filename[BUF_SIZE];
-    int a,b;
     e -> num_comandos++;
     printf("# %02d  Jogador:%d  Número de jogada:%d> ", e -> num_comandos , e -> jogador_atual , e -> num_jogadas);
     if(fgets(linha, BUF_SIZE, stdin) == NULL)
         return 0;
 
     if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
-        COORDENADA coord = {*col - 'a', *lin - '1'};
+        COORDENADA coord = {*lin - '1',*col - 'a'};
         jogar(e, coord);
         mostrar_tabuleiro(e);
     }
     if (sscanf (linha,"gr %s",filename) == 1){
         gravar (e,filename);
-        a= obter_numero_de_jogadas(e);
-        b = obter_jogador_atual(e) ;
     }
     if (sscanf (linha, "ler %s",filename) == 1){
-        ler (e,filename,a,b);
+        ler (e,filename);
     }
     char quit;
     if (sscanf (linha, "%[Q]" ,&quit) == 1){
@@ -116,8 +114,13 @@ int interpretador(ESTADO *e) {
         printf ("O jogo terminou.\n");
     }
     char arr[BUF_SIZE];
-    if (sscanf(linha,"%[movs]",arr)==1){
+    if (sscanf(linha,"%[movs]",arr) == 1){
         print_array (stdout,e);
+    }
+    int posx;
+    if (sscanf (linha,"pos %d",&posx) == 1) {
+        posicao (e,posx);
+        mostrar_tabuleiro(e);
     }
     return 1;
 }
