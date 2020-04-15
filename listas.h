ae7c3e7e482ebe792bb8950a97ffbbdad7f6a4b8
José João Cardoso Gonçalves   a93204 <61986130+jjgonc@users.noreply.github.com>
2020-04-15 18:12:57 +0100

    Update listas.h

--- listas.h
+++ listas.h
@@ -1,28 +1,72 @@
 //
 // Created by josejoao on 15/04/20.
 //
-
+/**
+ * @file listas.h
+ \brief funções relativas às listas ligadas.
+ */
 #ifndef PROJETO_LISTAS_H
 #define PROJETO_LISTAS_H
 
+/**
+ *\brief struct relativa à definição de uma lista.
+ */
 typedef struct nodo {
+    /** O valor referente ao nodo. */
     void *valor;
+    /** O seguimento da lista ligada. */
     struct nodo *proximo;
 } NODO, *LISTA;
 
 
 
-// Cria uma lista vazia
+
+/**
+ * função que cria uma lista vazia.
+ * @return lista vazia criada.
+ */
 LISTA criar_lista();
-// Insere um valor na cabeça da lista
+
+
+/**
+ * Função que insere um valor na cabeça de uma lista.
+ * @param L a lista.
+ * @param valor o valor a inserir.
+ * @return a nova lista com o valor na cabeça.
+ */
 LISTA insere_cabeca(LISTA L, void *valor);
-// Devolve a cabeça da lista
+
+
+
+/**
+ * Função que devolve a cabeça da lista.
+ * @param L a lista.
+ * @return a cabeça da lista.
+ */
 void *devolve_cabeca(LISTA L);
-// Devolve a cauda da lista
+
+
+/**
+ * Função que devolve a cauda da lista.
+ * @param L a lista.
+ * @return a cauda da lista.
+ */
 LISTA proximo(LISTA L);
-// Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
+
+
+/**
+ * Função que remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda.
+ * @param L a lista.
+ * @return a cauda da lista.
+ */
 LISTA remove_cabeca(LISTA L);
-// Devolve verdareiro se a lista é vazia
+
+
+/**
+ * Função que devolve verdareiro se a lista é vazia.
+ * @param L a lista.
+ * @return 0 caso seja falso e 1 caso seja verdadeiro.
+ */
 int lista_esta_vazia(LISTA L);
 
 #endif //PROJETO_LISTAS_H
