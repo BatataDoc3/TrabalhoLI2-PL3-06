f074c6b9bd441fe4a3f582c4faa306e044869ca8
José João Cardoso Gonçalves   a93204 <61986130+jjgonc@users.noreply.github.com>
2020-04-20 14:45:05 +0100

    Update bot.h

--- bot.h
+++ bot.h
@@ -2,5 +2,9 @@
 #define TRABALHOLI2_PL3_06_MASTER_4_BOT_H
 #include "estrutura.h"
 
+/**
+ * Função que executa o comando jogs.
+ * @param e o Estado.
+ */
 void jogs ( ESTADO *e);
 #endif //TRABALHOLI2_PL3_06_MASTER_4_BOT_H
