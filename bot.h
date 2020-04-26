cfb43e9dad70b565547da88ed7e1cae3abf582a7
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-04-26 23:36:11 +0100

    adição de funções para comando jog2

--- bot.h
+++ bot.h
@@ -1,10 +1,12 @@
 #ifndef TRABALHOLI2_PL3_06_MASTER_4_BOT_H
 #define TRABALHOLI2_PL3_06_MASTER_4_BOT_H
 #include "estrutura.h"
+typedef struct nodoArv {
+    COORDENADA valor;
+    struct nodoArv *CE , *CC , *CD , *DD , *BD , *BB, *BE , *EE;
+} *TREE ;
 
-/**
- * Função que executa o comando jogs.
- * @param e o Estado.
- */
 void jogs ( ESTADO *e);
+
+void jog2 (ESTADO *e);
 #endif //TRABALHOLI2_PL3_06_MASTER_4_BOT_H
