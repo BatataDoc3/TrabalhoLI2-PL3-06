50b410de31a59d39bfa17a30ef4eaf04065c787b
José João Cardoso Gonçalves   a93204 <61986130+jjgonc@users.noreply.github.com>
2020-04-30 13:15:37 +0100

    Update camadaInterface.h

--- camadaInterface.h
+++ camadaInterface.h
@@ -11,6 +11,39 @@
  */
 void mostrar_tabuleiro (ESTADO *e);
 
+
+/**
+ * Função auxiliar para a função gravar.
+ * @param f o ficheiro.
+ * @param e o estado.
+ */
+void gravar_tabuleiro (FILE *f,ESTADO *e);
+
+/**
+ * Função que dá print de um array.
+ * @param f o ficheiro.
+ * @param e o estado.
+ */
+void print_array (FILE *f, ESTADO *e);
+
+/**
+ * Função que grava o tabuleiro (comando gr).
+ * @param e o estado.
+ * @param filename o nome do ficheiro.
+ * @return
+ */
+int gravar (ESTADO *e,char *filename);
+
+
+/**
+ * Função que lê um tabuleiro.
+ * @param e o estado.
+ * @param filename o nome do ficheiro.
+ * @return
+ */
+int ler (ESTADO *e,char *filename);
+
+
 /**
  * Função que lê os inputs e efetua as Jogadas no tabuleiro.
  * @param e Apontador para o ESTADO.
