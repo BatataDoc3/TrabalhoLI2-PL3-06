17a8ffb9475c0c30a1a0a66d0977627295c5812c
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-05-02 21:26:07 +0100

    versão final

--- projeto/camadaInterface.c
+++ projeto/camadaInterface.c
@@ -1,4 +1,4 @@
-#include "logicaProg.h"
+include "logicaProg.h"
 #include "estrutura.h"
 #include <stdio.h>
 #include <string.h>
@@ -11,10 +11,10 @@ void mostrar_tabuleiro (ESTADO *e) {
         putchar (letra) ;
         letra--;
         for (coluna = 0; coluna < 8; coluna ++) {
-            if (obter_estado_casa(e,linha,coluna) == DOIS) printf (" 2");
-            else if (obter_estado_casa(e,linha,coluna) == UM) printf (" 1");
-            else if (obter_estado_casa(e,linha,coluna)  == VAZIO) printf (" .") ;
-            else if (obter_estado_casa(e,linha,coluna)  == BRANCA) printf (" *");
+            if (obter_estado_casa (e,linha,coluna) == DOIS) printf (" 2");
+            else if (obter_estado_casa (e,linha,coluna) == UM) printf (" 1");
+            else if (obter_estado_casa (e,linha,coluna)  == VAZIO) printf (" .") ;
+            else if (obter_estado_casa (e,linha,coluna)  == BRANCA) printf (" *");
             else printf (" #");
         }
         printf ("\n");
@@ -22,15 +22,14 @@ void mostrar_tabuleiro (ESTADO *e) {
     printf ("  A B C D E F G H \n");
 }
 
-
 void gravar_tabuleiro (FILE *f,ESTADO *e) {
     int linha,coluna;
     for (linha = 7; linha >= 0; linha--) {
         for (coluna = 0; coluna < 8; coluna ++) {
-            if (obter_estado_casa(e,linha,coluna) == DOIS) fprintf (f,"2");
-            else if (obter_estado_casa(e,linha,coluna) == UM) fprintf (f,"1");
-            else if (obter_estado_casa(e,linha,coluna)  == VAZIO) fprintf (f,".") ;
-            else if (obter_estado_casa(e,linha,coluna)  == BRANCA) fprintf (f,"*");
+            if (obter_estado_casa (e,linha , coluna) == DOIS) fprintf (f,"2");
+            else if (obter_estado_casa (e, linha, coluna) == UM) fprintf (f,"1");
+            else if (obter_estado_casa (e, linha, coluna)  == VAZIO) fprintf (f,".") ;
+            else if (obter_estado_casa (e, linha, coluna)  == BRANCA) fprintf (f,"*");
             else fprintf (f,"#");
         }
         fprintf (f,"\n");
@@ -41,23 +40,23 @@ void print_array (FILE *f, ESTADO *e){
     int i;
     for (i=0;i <= (obter_numero_de_jogadas(e)); i++ ){
         if (i<9){
-            if ((e->jogadas[i].jogador1.linha)!= 0 || e->jogadas[i].jogador1.coluna != 0) {
-                fprintf(f, "0%d: %c%d", i + 1, (e->jogadas[i].jogador1.coluna) + 'a', e->jogadas[i].jogador1.linha + 1);
+            if ((e -> jogadas[i].jogador1.linha)!= 0 || e -> jogadas[i].jogador1.coluna != 0) {
+                fprintf(f, "0%d: %c%d", i + 1, (e -> jogadas[i].jogador1.coluna) + 'a', e -> jogadas[i].jogador1.linha + 1);
             }
-            if ((e->jogadas[i].jogador2.linha)!= 0 || e->jogadas[i].jogador2.coluna != 0) {
-                fprintf(f, " %c%d ", (e->jogadas[i].jogador2.coluna) + 'a', e->jogadas[i].jogador2.linha + 1);
+            if ((e -> jogadas[i].jogador2.linha)!= 0 || e -> jogadas[i].jogador2.coluna != 0) {
+                fprintf(f, " %c%d ", (e -> jogadas[i].jogador2.coluna) + 'a', e -> jogadas[i].jogador2.linha + 1);
                 fprintf(f, "\n");
             }
-            if (((e->jogadas[i].jogador1.linha)!= 0 || e->jogadas[i].jogador1.coluna != 0) && ((e->jogadas[i].jogador2.linha)== 0 && e->jogadas[i].jogador2.coluna == 0)){
+            if (((e -> jogadas[i].jogador1.linha)!= 0 || e -> jogadas[i].jogador1.coluna != 0) && ((e -> jogadas[i].jogador2.linha)== 0 && e -> jogadas[i].jogador2.coluna == 0)){
                 fprintf(f, "\n");
             }
         }
         else {
-            if ((e->jogadas[i].jogador1.linha)!= 0 || e->jogadas[i].jogador1.coluna != 0) {
-                fprintf(f, "%d: %c%d", i + 1, (e->jogadas[i].jogador1.coluna) + 'a', e->jogadas[i].jogador1.linha + 1);
+            if ((e -> jogadas[i].jogador1.linha)!= 0 || e -> jogadas[i].jogador1.coluna != 0) {
+                fprintf(f, "%d: %c%d", i + 1, (e -> jogadas[i].jogador1.coluna) + 'a', e -> jogadas[i].jogador1.linha + 1);
             }
-            if ((e->jogadas[i].jogador2.linha)!= 0 || e->jogadas[i].jogador2.coluna != 0) {
-                fprintf(f, " %c%d ", (e->jogadas[i].jogador2.coluna) + 'a', e->jogadas[i].jogador2.linha + 1);
+            if ((e -> jogadas[i].jogador2.linha)!= 0 || e -> jogadas[i].jogador2.coluna != 0) {
+                fprintf(f, " %c%d ", (e -> jogadas[i].jogador2.coluna) + 'a', e -> jogadas[i].jogador2.linha + 1);
                 fprintf(f, "\n");
             }
             else {
@@ -71,10 +70,10 @@ int gravar (ESTADO *e,char *filename) {
     FILE *f = fopen (filename,"w");
     if (f == NULL)
         return 0;
-    gravar_tabuleiro(f,e);
+    gravar_tabuleiro (f,e);
     fprintf (f,"\n");
-    print_array(f,e);
-    fclose(f);
+    print_array (f,e);
+    fclose (f);
     return 1;
 }
 
@@ -84,8 +83,8 @@ int ler (ESTADO *e,char *filename){
         printf ("Erro ao abrir ficheiro\n");
         return 0;
     }
-    le_ficheiro(e, f);
-    mostrar_tabuleiro(e);
+    le_ficheiro (e, f);
+    mostrar_tabuleiro (e);
     return 1;
 }
 
@@ -99,8 +98,8 @@ int interpretador(ESTADO *e) {
         return 0;
     if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
         COORDENADA coord = {*lin - '1', *col - 'a'};
-        jogar(e, coord);
-        mostrar_tabuleiro(e);
+        jogar (e, coord);
+        mostrar_tabuleiro (e);
     }
     if (sscanf (linha,"gr %s",filename) == 1){
         gravar (e,filename);
@@ -125,11 +124,10 @@ int interpretador(ESTADO *e) {
         jogs (e);
         mostrar_tabuleiro(e);
     }
-     if (strcmp (linha,"jog2\n") == 0) {
-        jog2 (e);  
+    if (strcmp (linha,"jog2\n") == 0) {
+        jog2 (e);
         mostrar_tabuleiro(e);
     }
     else return 0;
     return 1;
 }
-
