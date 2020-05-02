528bd9ad9f63dbc3dfdb1ebbd45733936f4cba88
José João Cardoso Gonçalves   a93204 <61986130+jjgonc@users.noreply.github.com>
2020-05-02 17:13:27 +0100

    Update arvores.h

--- arvores.h
+++ arvores.h
@@ -1,5 +1,5 @@
-#ifndef TRABALHOLI2_PL3_06_MASTER_8_2_CÓPIA_ARVORES_H
-#define TRABALHOLI2_PL3_06_MASTER_8_2_CÓPIA_ARVORES_H
+#ifndef PROJETO_LI2_ARVORES_H
+#define PROJETO_LI2_ARVORES_H
 #include "estrutura.h"
 
 /**
@@ -21,14 +21,45 @@ typedef struct nodoArv {
 } *TREE ;
 
 
+/**
+ * Função que cria uma árvore.
+ * @param e o estado.
+ * @param linha a linha.
+ * @param coluna a coluna.
+ * @param profundidade a profundidade da árvore. (número de nodos)
+ * @return
+ */
 TREE criar_arvore (ESTADO *e,int linha,int coluna,int profundidade);
 
+/**
+ * Função que calcula o minimo de um array de 8 elementos.
+ * @param a a árvore.
+ * @param n o tamanho do array.
+ * @return
+ */
 float min8 (float *a,int n);
 
+/**
+ * Função que calcula o maximo de um array de 8 elementos.
+ * @param a a arvore.
+ * @param n o tamanho do array.
+ * @return
+ */
 float max8  (float *a,int n);
 
-float preencher_class (TREE arvore,int profundidade);   //FIXME
+/**
+ * Preenche os nodos da árvore com as classificações utilizando a heurística MinMax.
+ * @param arvore a árvore.
+ * @param profundidade a profundidade.
+ * @return
+ */
+float preencher_class (TREE arvore,int profundidade);
 
+/**
+ * Função que liberta o espaço ocupado apos a criação de uma árvore.
+ * @param arvore a árvore.
+ * @param profundidade a profundidade.
+ */
 void free_arvore (TREE arvore,int profundidade);
 
 #endif //TRABALHOLI2_PL3_06_MASTER_8_2_CÓPIA_ARVORES_H
