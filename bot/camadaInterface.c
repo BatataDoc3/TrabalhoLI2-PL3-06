6da4260959a6c528c57642b0c3145acb9fdecd2b
Rui Filipe Coelho Moreira -a93232 <61985785+RuiMoreiraA93232@users.noreply.github.com>
2020-05-03 17:24:44 +0100

    Update camadaInterface.c

--- bot/camadaInterface.c
+++ bot/camadaInterface.c
@@ -3,25 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "arvores.h"
-/*
-void mostrar_tabuleiro (ESTADO *e) {
-    int linha,coluna;
-    char letra = '8';
-    for (linha = 7; linha >= 0; linha--) {
-        putchar (letra) ;
-        letra--;
-        for (coluna = 0; coluna < 8; coluna ++) {
-            if (obter_estado_casa (e,linha,coluna) == DOIS) printf (" 2");
-            else if (obter_estado_casa (e,linha,coluna) == UM) printf (" 1");
-            else if (obter_estado_casa (e,linha,coluna)  == VAZIO) printf (" .") ;
-            else if (obter_estado_casa (e,linha,coluna)  == BRANCA) printf (" *");
-            else printf (" #");
-        }
-        printf ("\n");
-    }
-    printf ("  A B C D E F G H \n");
-}
-*/
+
 void gravar_tabuleiro (FILE *f,ESTADO *e) {
     int linha,coluna;
     for (linha = 7; linha >= 0; linha--) {
@@ -84,48 +66,3 @@ int ler (ESTADO *e,char *filename){
     le_ficheiro(e, f);
     return 1;
 }
-/*
-int interpretador(ESTADO *e) {
-    char linha[BUF_SIZE];
-    char col[2], lin[2];
-    char filename[BUF_SIZE];
-    e -> num_comandos++;
-    printf("# %02d  Jogador:%d  Número de jogada:%d> ", e -> num_comandos , e -> jogador_atual , e -> num_jogadas);
-    if(fgets(linha, BUF_SIZE, stdin) == NULL)
-        return 0;
-    if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
-        COORDENADA coord = {*lin - '1', *col - 'a'};
-        jogar(e, coord);
-        mostrar_tabuleiro(e);
-    }
-    if (sscanf (linha,"gr %s",filename) == 1){
-        gravar (e,filename);
-    }
-    if (sscanf (linha, "ler %s",filename) == 1){
-        ler (e,filename);
-    }
-    if(strcmp(linha, "Q\n") == 0) {
-        e->num_jogadas=32;
-        printf ("O jogo terminou.\n");
-    }
-    if(strcmp(linha, "movs\n") == 0) {
-        print_array(stdout, e);
-    }
-    int x;
-    if (sscanf (linha,"pos %d",&x) == 1) {
-        e -> posx = x;
-        atualizar_tabuleiro_jogadas (e);
-        mostrar_tabuleiro(e);
-    }
-    if (strcmp (linha,"jog\n") == 0) {
-        jogs (e);
-        mostrar_tabuleiro(e);
-    }
-    if (strcmp (linha,"jog2\n") == 0) {
-        jog2 (e);
-        mostrar_tabuleiro(e);
-    }
-    else return 0;
-    return 1;
-}
-*/
